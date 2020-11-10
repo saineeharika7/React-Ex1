ecbb4aa18eca24fe463a0c19aa3b5f2bb946558d
neeharika <saineeharikaneha@gmail.com>
2020-11-10 11:44:04 +0530

    conditional-render

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -9,15 +9,31 @@ class UserGreeting extends Component {
     }
     
     render() {
-        if(this.state.isLoggedIn != true){  
+
+        /*let message;
+        if(this.state.isLoggedIn){
+            message =<div>WELCOME root !!</div>
+        }
+        else{
+            message =<h1>hello everyone </h1>
+        } 
+        return(
+             <div>{message}</div>
+         ) */
+        /*if(this.state.isLoggedIn != true){  
             return <div>welcome Root !!</div>
 
           }
           else{
                   return <div>welcome Guest !!</div>
 
-          }
-       
+          }*/
+        /*return(
+            !this.state.isLoggedIn ? <h1>hello root </h1> : <h1>hello everyone </h1>
+        )*/
+        return(
+            !this.state.isLoggedIn && <h1>welcome shaw</h1>
+        ) 
     }
 }
 
